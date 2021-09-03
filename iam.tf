90ddec5670480a45bb8e5cc31e60750f9a4db863
Luca Silvestri <43203075+silvestriluca@users.noreply.github.com>
2021-09-03 17:18:43 +0200

    Added iac manager role & policy

--- iam.tf
+++ iam.tf
@@ -1,8 +1,9 @@
-resource "aws_iam_user" "base_infrastructure_terraform_user" {
-  name = "TerraformBaseInfra"
+resource "aws_iam_role" "baseline_iac_manager" {
+  name_prefix = "baseline_iac_manager"
+  description = "Role to manage baseline infrastructure with Terraform"
 }
 
-resource "aws_iam_user_policy_attachment" "remote_state_access" {
-  user = aws_iam_user.base_infrastructure_terraform_user.name
+resource "aws_iam_role_policy_attachment" "name" {
+  role       = aws_iam_role.baseline_iac_manager.name
   policy_arn = module.s3_remote_state.terraform_iam_policy.arn
 }

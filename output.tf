4bc86f51019e2dccea67e7e344a207c55acc4326
Luca Silvestri <43203075+silvestriluca@users.noreply.github.com>
2021-09-09 16:39:30 +0200

    Added more outputs

--- output.tf
+++ output.tf
@@ -1,7 +1,24 @@
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
+output "account-id" {
+  value       = data.aws_caller_identity.current.account_id
+  description = "AWS acoount where infrastructure is deployed"
 }
 
 output "tf-workspace" {
-  value = terraform.workspace
+  value       = terraform.workspace
+  description = "TF workspace where state is persisted"
+}
+
+output "state-bucket" {
+  value       = module.s3_remote_state.state_bucket.bucket
+  description = "The S3 bucket name to store the remote state file"
+}
+
+output "state-bucket-replica" {
+  value       = module.s3_remote_state.replica_bucket.bucket
+  description = "The S3 bucket name to replicate the state S3 bucket"
+}
+
+output "state-bucket-kms-id" {
+  value       = module.s3_remote_state.kms_key.id
+  description = "The KMS customer master key ID to encrypt state buckets"
 }

1806418bae89269de9143a348318927ba0ea2e00
Luca Silvestri <43203075+silvestriluca@users.noreply.github.com>
2021-09-09 15:10:19 +0200

    Added workspace awareness in naming

--- state-infrastructure.tf
+++ state-infrastructure.tf
@@ -6,19 +6,19 @@ module "s3_remote_state" {
     aws.replica = aws.replica
   }
 
-  dynamodb_table_name            = "tf-rstate-lock-baseline"
+  dynamodb_table_name            = "tf-rstate-lock-bl-${terraform.workspace}"
   s3_bucket_force_destroy        = true
-  state_bucket_prefix            = "tf-rstate-baseline"
-  replica_bucket_prefix          = "tf-rstate-replica-baseline"
+  state_bucket_prefix            = "tf-rstate-bl-${terraform.workspace}"
+  replica_bucket_prefix          = "tf-rstate-replica-bl-${terraform.workspace}"
   noncurrent_version_expiration  = null
   noncurrent_version_transitions = []
 
-  terraform_iam_policy_name_prefix = "tf-baseline"
-  iam_policy_attachment_name       = "tf-iam-role-attachment-repl-configuration-baseline"
-  iam_policy_name_prefix           = "tf-rstate-repl-policy-baseline"
-  iam_role_name_prefix             = "tf-rstate-repl-role-baseline"
+  terraform_iam_policy_name_prefix = "tf-bl-${terraform.workspace}"
+  iam_policy_attachment_name       = "tf-iam-role-attachment-repl-configuration-bl-${terraform.workspace}"
+  iam_policy_name_prefix           = "tf-rstate-repl-policy-bl-${terraform.workspace}"
+  iam_role_name_prefix             = "tf-rstate-repl-role-bl-${terraform.workspace}"
 
-  kms_key_description = "The key used to encrypt the remote state bucket - baseline."
+  kms_key_description = "The key used to encrypt the remote state bucket - bl-${terraform.workspace}."
 
   tags = local.global_tags
 }

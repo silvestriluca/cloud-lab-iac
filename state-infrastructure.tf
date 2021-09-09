9e7984fa2b08896e9dfc759c1df0d99eb6786800
Luca Silvestri <43203075+silvestriluca@users.noreply.github.com>
2021-09-09 15:36:48 +0200

    Added KMS alias and changed prefixes

--- state-infrastructure.tf
+++ state-infrastructure.tf
@@ -8,17 +8,22 @@ module "s3_remote_state" {
 
   dynamodb_table_name            = "tf-rstate-lock-bl-${terraform.workspace}"
   s3_bucket_force_destroy        = true
-  state_bucket_prefix            = "tf-rstate-bl-${terraform.workspace}"
-  replica_bucket_prefix          = "tf-rstate-replica-bl-${terraform.workspace}"
+  state_bucket_prefix            = "tf-rstate-bl-${terraform.workspace}-"
+  replica_bucket_prefix          = "tf-rstate-replica-bl-${terraform.workspace}-"
   noncurrent_version_expiration  = null
   noncurrent_version_transitions = []
 
-  terraform_iam_policy_name_prefix = "tf-bl-${terraform.workspace}"
-  iam_policy_attachment_name       = "tf-iam-role-attachment-repl-configuration-bl-${terraform.workspace}"
-  iam_policy_name_prefix           = "tf-rstate-repl-policy-bl-${terraform.workspace}"
-  iam_role_name_prefix             = "tf-rstate-repl-role-bl-${terraform.workspace}"
+  terraform_iam_policy_name_prefix = "tf-bl-${terraform.workspace}-"
+  iam_policy_attachment_name       = "tf-iam-role-attachment-repl-configuration-bl-${terraform.workspace}-"
+  iam_policy_name_prefix           = "tf-rstate-repl-policy-bl-${terraform.workspace}-"
+  iam_role_name_prefix             = "tf-rstate-repl-role-bl-${terraform.workspace}-"
 
-  kms_key_description = "The key used to encrypt the remote state bucket - bl-${terraform.workspace}."
+  kms_key_description = "The key used to encrypt the remote state bucket / bl-${terraform.workspace}."
 
   tags = local.global_tags
+}
+
+resource "aws_kms_alias" "state_bucket_key" {
+  name          = "alias/bucket-tf-rstate-bl-${terraform.workspace}"
+  target_key_id = module.s3_remote_state.kms_key.id
 }
